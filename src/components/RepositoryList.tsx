79f7db39e642ffa4e9a293b0043cc2d055b51392
Rodrigo Jacomeli <rodrigo.devvga@gmail.com>
2021-08-31 19:54:48 -0300

    Commit - Leasson 20 - Ignite - Translate archives from TSX + Add types

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../style/Repositories.scss";
 
+interface IRepository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<IRepository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
@@ -18,7 +24,9 @@ export function RepositoryList() {
 
       <ul>
         {repositories.map((repository) => {
-          return <RepositoryItem key={repository.name} repository={repository} />;
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
       </ul>
     </section>
